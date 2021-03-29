de383644a6d729ef52f2f07faa16aa65104485f5
Patrik Cerny <loplok12345@gmail.com>
2021-03-30 01:33:47 +0200

    registration, login handling

---
+++ signin_menu.php
@@ -0,0 +1,150 @@
+<?php
+require_once "config.php";
+ 
+$username = $password = $confirm_password = $email = "";
+$username_err = $password_err = $confirm_password_err = $email_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+
+        if($stmt = $mysqli->prepare($sql)){
+            $stmt->bind_param("s", $param_username);
+            $param_username = trim($_POST["username"]);        
+            if($stmt->execute()){
+                // store result
+                $stmt->store_result();
+                
+                if($stmt->num_rows == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            $stmt->close();
+        }
+    }
+
+     // Validate email
+    if(empty(trim($_POST["email"]))){
+        $username_err = "Please enter an email.";
+    } else {
+        $sql = "SELECT email FROM users WHERE username = ?";
+        
+        if($stmt = $mysqli->prepare($sql)){
+            $stmt->bind_param("s", $param_email);
+            $param_email = trim($_POST["email"]);
+
+            if($stmt->execute()){
+                // store result
+                $stmt->store_result();
+                
+                if($stmt->num_rows == 1){
+                    $email_err = "This email is already taken.";
+                } else{
+                    $email = trim($_POST["email"]);
+                    echo $email;
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+            $stmt->close();
+        }
+    }
+
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";      
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)){
+        $sql = "INSERT INTO `users`(id, username, created, email, password) VALUES (NULL, ?, NOW(),  ?, ?)";
+        if($stmt = $mysqli->prepare($sql)){
+            $stmt->bind_param("sss", $param_username, $param_email, $param_password);
+            
+            $param_username = $username;
+            $param_email = $email;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            if($stmt->execute()){
+                header("location: main_menu.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+                echo $stmt->error; 
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+    
+    // Close connection
+    $mysqli->close();
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div> 
+            <div class="form-group">
+                <label>E-mail</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>   
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group subm-btn">
+                <input type="submit" class="btn btn-primary" value="Submit">
+            </div>
+            <div class="form-group reset-btn">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login_menu.php"> Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
