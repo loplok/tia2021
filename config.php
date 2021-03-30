2decc477f31088394b23bdb9f494e124f7ab8e74
Patrik Černý <loplok12345@gmail.com>
2021-03-31 00:21:47 +0200

    updates secrets so config does not have db access

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'db-mysql-fra1-36602-do-user-8828141-0.b.db.ondigitalocean.com:25060');
-define('DB_USERNAME', 'doadmin');
-define('DB_PASSWORD', 'wwto58zqjtwnk5m0');
-define('DB_NAME', 'defaultdb');
+define('DB_SERVER', '${{secrets.DB_SERVER}}');
+define('DB_USERNAME', '${{secrets.DB_USERNAME}}');
+define('DB_PASSWORD', '${{secrets.DB_PASSWORD}}');
+define('DB_NAME', '${{secrets.DB_NAME}}');
  
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
